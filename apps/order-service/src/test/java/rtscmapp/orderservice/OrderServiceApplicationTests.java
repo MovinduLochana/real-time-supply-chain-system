502b41329376fec346ab835c145b6082dadba0df
MovinduLochana <www.movindu@gmail.com>
2025-07-05 01:02:27 +0530

    Inital commit for order-service

---
+++ apps/order-service/src/test/java/rtscmapp/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rtscmapp.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
